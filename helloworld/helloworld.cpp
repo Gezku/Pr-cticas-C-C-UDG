9851dce87727d3222754c0fb82bfc9455870daaf
Gezku <onlyautumnshade@gmail.com>
2018-09-14 01:22:31 -0500

    Initial commit

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,17 @@
+/*
+ * helloworld.cpp
+ *
+ *  Created on: 14/09/2018
+ *      Author: Fernando
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+
